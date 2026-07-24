8d9b0368ee449322151668fe2710df52daa11130
Borra Keerthi <borrakeerthi13@gmail.com>
2026-07-24 19:07:46 +0530

    Add pattern8 function to print upside-down pyramid
    Implement pattern printing function with spaces and stars.

---
+++ Patterns/pattern8.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void pattern8(int n){
+    for(int i=n-1;i>=0;i--){
+        //To print spaces
+        for(int j=0;j<n-1-i;j++){
+            cout<<"  ";
+        }
+        //To print Stars
+        for(int j=0;j<2*i+1;j++){
+            cout<<"* ";
+        }
+        //Again to print space
+        for(int j=0;j<n-1-i;j++){
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the value of n:";
+    cin>>n;
+    pattern7(n);
+    return 0;
+}
