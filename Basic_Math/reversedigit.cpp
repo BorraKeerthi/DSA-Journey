b24465dfa0d9d718cdceacf6930cbd81fde6381f
Borra Keerthi <borrakeerthi13@gmail.com>
2026-07-29 22:42:03 +0530

    Add digit reversal program in reversedigit.cpp
    Implement a program to reverse the digits of an integer.

---
+++ Basic_Math/reversedigit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n:";
+    cin>>n;
+    int digit,rev;
+    while(n>0){
+       digit=n%10;
+       rev=rev*10+digit;
+       n=n/10;
+    }
+    cout<<rev;
+    return 0;
+}
