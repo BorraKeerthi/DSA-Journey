8bdb9ef8b64534d80f4e3216fe3461b33e7c90c0
Borra Keerthi <borrakeerthi13@gmail.com>
2026-08-13 23:49:00 +0530

    Add selection sort implementation in C++

---
+++ sort/selectionsort.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int Selectionsort(int n,int arr[]){
+    for(int i=0;i<=n-2;i++){
+        int min=i;
+        for(int j=i;j<=n-1;j++){
+            if(arr[j]<arr[min]){
+                min=j;
+            }
+        }
+        swap(arr[min],arr[i]);
+    }
+    return 0;
+}
+int main(){
+    int n;
+    cout<<"Enter the size:";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    Selectionsort(n,arr);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
