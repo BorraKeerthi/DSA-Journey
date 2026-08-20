7370e42c0279a78fd00235088253cee71234d252
Borra Keerthi <borrakeerthi13@gmail.com>
2026-08-20 23:32:23 +0530

    Add Dutch National Flag sorting algorithm
    Implement Dutch National Flag algorithm to sort an array of 0s, 1s, and 2s.

---
+++ Array/Dutch_National_flag.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int sorts(vector<int>&arr,int n){
+    int low=0;
+    int mid=0;
+    int high=n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr[low],arr[mid]);
+            mid++;
+            low++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }
+        else{
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+    return 0;
+}
+int main(){
+    int n;
+    cout<<"Enter the value of n:";
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sorts(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i];
+    }
+}
