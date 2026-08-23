2ee2744cbb458026af33f98c83f88a476595c4ba
Borra Keerthi <borrakeerthi13@gmail.com>
2026-08-23 22:26:13 +0530

    longest consecutive sequence

--- Array/longest_consecutive_sequnece.cpp
+++ Array/longest_consecutive_sequnece.cpp
@@ -1,21 +1,28 @@
-int largestarray(vector<int>&nums,int n){
-    if(n==0){
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0){
         return 0;
     }
     int cnt=0;
     int largest=1;
-    int lastsmallest=INT_MIN;
     sort(nums.begin(),nums.end());
+    int lastsmallest=nums[0]-1;
     for(int i=0;i<n;i++){
         if(nums[i]-1==lastsmallest){
             cnt++;
             lastsmallest=nums[i];
         }
-        else if(nums[i]-1!=lastsmallest){
+        else if (nums[i]==lastsmallest){
+            continue;
+        }
+        else {
             cnt=1;
             lastsmallest=nums[i];
         }
         largest=max(largest,cnt);
     }
     return largest;
-}
+    }
+};
