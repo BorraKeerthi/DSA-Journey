1b2accce15181b6844d452bf5e8617871a4b7edd
Borra Keerthi <borrakeerthi13@gmail.com>
2026-08-25 20:15:26 +0530

    Add Pascal's triangle generator class
    Implement Pascal's triangle generation using a class.

---
+++ Array/pascal_triangle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>>temp;
+        for(int row=1;row<=numRows;row++){
+            vector<int>ansRow;
+            long long ans=1;
+            ansRow.push_back(1);
+            for(int col=1;col<row;col++){
+                ans=ans*(row-col);
+                ans=ans/col;
+                ansRow.push_back(ans);
+            }
+            temp.push_back(ansRow);
+        }
+        return temp;
+    }
+};
