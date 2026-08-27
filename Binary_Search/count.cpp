3be77765a785aef4e1f0b02fca1851ba99763e7c
Borra Keerthi <borrakeerthi13@gmail.com>
2026-08-27 16:34:21 +0530

    Implement count function for target occurrences

---
+++ Binary_Search/count.cpp
@@ -0,0 +1,12 @@
+vector<int> count(vector<int>& nums, int target) {
+    int first=-1;
+    int last=-1;
+    int cnt=0;
+    first=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+    if (first == nums.size() || nums[first] != target) {
+        return {-1, -1};
+    }
+    last=upper_bound(nums.begin(),nums.end(),target)-nums.begin()-1;
+    int cnt=last-first+1;
+    return {first,last};
+}
