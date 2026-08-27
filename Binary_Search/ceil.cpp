8f50bdec4bb4efc82db21b0ecdddefbd1aa8555b
Borra Keerthi <borrakeerthi13@gmail.com>
2026-08-27 15:49:11 +0530

    Add ceil function for binary search
    Implement binary search to find the ceiling of a number in a sorted array.

---
+++ Binary_Search/ceil.cpp
@@ -0,0 +1,16 @@
+int ceil(vector<int>&arr,int n,int x){
+    int low=0;
+    int high=n-1;
+    int ans=-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(arr[mid]>=x){
+            ans=arr[mid];
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
